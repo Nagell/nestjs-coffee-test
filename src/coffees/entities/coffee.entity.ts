59882407604f1d95dc0637adcdbf49f30f5ccc9a
Dawid <dawidnitka@gmail.com>
2022-04-16 18:40:10 +0200

    added postgres

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity() // automatically creates table in database ('coffee' in this case)
 export class Coffee {
+    @PrimaryGeneratedColumn()
     id: number
+
+    @Column()
     name: string
+
+    @Column()
     brand: string
+
+    @Column('json', { nullable: true })
     flavours: string[]
 }
