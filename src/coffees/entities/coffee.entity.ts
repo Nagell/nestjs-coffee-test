7b4ad211b4c794aa8ffbe7b0f0c1854f4043d277
Dawid <dawidnitka@gmail.com>
2022-04-21 00:07:45 +0200

    added Flavors

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm"
+import { Flavor } from "./flavor.entity"
 
 @Entity() // automatically creates table in database ('coffee' in this case)
 export class Coffee {
@@ -11,6 +12,13 @@ export class Coffee {
     @Column()
     brand: string
 
-    @Column('json', { nullable: true })
-    flavours: string[]
+    @JoinTable()
+    @ManyToMany(
+        type => Flavor,
+        flavor => flavor.coffees,
+        {
+            cascade: true,
+        }
+    )
+    flavors: Flavor[]
 }
