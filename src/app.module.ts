86ae1d7bf9e78139b43c662dcf315f42dd957774
Dawid <dawidnitka@gmail.com>
2022-06-01 23:26:59 +0200

    added async TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,22 +11,24 @@ import appConfig from './config/app.config'
 
 @Module({
     imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: () => ({
+                type: 'postgres',
+                host: process.env.DB_HOST,
+                port: +process.env.DB_PORT,
+                username: process.env.DB_USER,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_NAME,
+                autoLoadEntities: true,
+                synchronize: true, // TODO: disable it for production
+            }),
+        }),
         ConfigModule.forRoot({
             // validationSchema: Joi.object({
             //     DB_HOST: Joi.string().required(),
             //     DB_PORT: Joi.number().default(5432),
             // }),
             load: [appConfig]
-        }),
-        TypeOrmModule.forRoot({
-            type: 'postgres',
-            host: process.env.DB_HOST,
-            port: +process.env.DB_PORT,
-            username: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            autoLoadEntities: true,
-            synchronize: true, // TODO: disable it for production
         }),
         CoffeesModule,
         CoffeeRatingModule,
